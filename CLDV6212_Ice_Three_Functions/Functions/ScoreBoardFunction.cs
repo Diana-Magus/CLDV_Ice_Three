40c73f0ad9df5abab2e099f1f0fa920a61539bbd
Hannah Terblanche <126674584+Diana-Magus@users.noreply.github.com>
2024-09-03 17:32:43 +0200

    created Scorebaord fuinction

---
+++ CLDV6212_Ice_Three_Functions/Functions/ScoreBoardFunction.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace CLDV6212_Ice_Three_Functions.Functions
+{
+    public class ScoreBoardFunction
+    {
+        private readonly ILogger<ScoreBoardFunction> _logger;
+
+        public ScoreBoardFunction(ILogger<ScoreBoardFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("ScoreBoardFunction")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
